534033214f535c298fc804a5eb63631e38c6d24c
SLEEPwalker131313 <petrunin-vn@yandex.ru>
2015-11-15 18:01:21 +0300

    Profiles
    Добавлена возможность включать\выключать по отдельности каждый из трёх
    профилей.

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@
 
 int main()
 {
-	int k = -1;
-	int choice = -1;
-	std::string s = "tmp";
-	std::string res = "netsh advfirewall set currentprofile logging filename \"";
+	int k = -1;							//Choice menu var	
+	int choice = -1;					//sure var
+	std::string s = "tmp";				//string IN
+	std::string res = "netsh advfirewall set currentprofile logging filename \"";	//string result log
 	
 
 	while (k != 0)
@@ -25,6 +25,9 @@ int main()
 		std::cout << "***\t2 - WinFirewall Off" << std::endl;
 		std::cout << "***\t3 - WinFirewall Reset" << std::endl;
 		std::cout << "***\t4 - Change the path to save the logs" << std::endl;
+		std::cout << "***\t5 - Inclusion separate profiles" << std::endl;
+		std::cout << "***\t6 - Disable separate profiles" << std::endl;
+
 		std::cout << "***\t0 - EXIT" << std::endl << std::endl;
 
 		std::cin >> k;
@@ -39,26 +42,77 @@ int main()
 			break;
 		case 3:
 			std::cout << "Sure? 1 - yes, any other - no" << std::endl;
-			if(std::cin >> choice)
+			if (std::cin >> choice)
+			if (choice == 1)
+			{
+				std::cout << "Are you sure? 1 - yes, any other - no" << std::endl;
+				std::cin >> choice;
 				if (choice == 1)
 				{
-					std::cout << "Are you sure? 1 - yes, any other - no" << std::endl;
-					std::cin >> choice;
-					if (choice == 1)
-					{
-						system("netsh advfirewall reset");
-					}
+					system("netsh advfirewall reset");
 				}
-				break;
+			}
+			choice = -1;
+			break;
 		case 4:
 			std::cout << "enter the new path: example(C:\\\temp\\\pfirewall.log)\nYour new path: ";
 			std::getline(std::cin, s);
 			res += s;
 			res += "\"";
 			std::cout << "\n" << res << "\n";
-			//Äîáàâü êîìàíäó netsh advfirewall set currentprofile logging filename «C:\temp\pfirewall.log»
+			//Add command netsh advfirewall set currentprofile logging filename «C:\temp\pfirewall.log»
 			break;
+		case 5:
+			while (choice != 1 && choice != 2 && choice != 3 && choice != 0)
+			{
+				std::cout << "1 - Domain" << std::endl;
+				std::cout << "2 - Private" << std::endl;
+				std::cout << "3 - Public" << std::endl;
+				std::cout << "0 - Back to menu" << std::endl;
 
+				if (std::cin >> choice)
+				{
+					if (choice == 1)
+					{
+						system("netsh advfirewall set domainprofile state on");
+					}
+					else if (choice == 2)
+					{
+						system("netsh advfirewall set privateprofile state on");
+					}
+					else if (choice == 3)
+					{
+						system("netsh advfirewall set publicprofile state on");
+					}
+				}
+			}
+			choice = -1;
+			break;
+		case 6:
+			while (choice != 1 && choice != 2 && choice != 3 && choice != 0)
+			{
+				std::cout << "1 - Domain" << std::endl;
+				std::cout << "2 - Private" << std::endl;
+				std::cout << "3 - Public" << std::endl;
+				std::cout << "0 - Back to menu" << std::endl;
+
+				if (std::cin >> choice)
+				{
+					if (choice == 1)
+					{
+						system("netsh advfirewall set domainprofile state off");
+					}
+					else if (choice == 2)
+					{
+						system("netsh advfirewall set privateprofile state off");
+					}
+					else if (choice == 3)
+					{
+						system("netsh advfirewall set publicprofile state off");
+					}
+				}
+			}
+			break;
 		}
 	}
 	return 0;
